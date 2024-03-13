2329d6b010b8b98c759da19da6655371cda44684
Pradnyanand Jade <pradnyanand.jade@walchandsangli.ac.in>
2024-03-13 17:44:59 +0530

    Initial commit_Jade_02

--- app/src/main/java/com/miniproject/attendx/login.kt
+++ app/src/main/java/com/miniproject/attendx/login.kt
@@ -1,25 +1,24 @@
 package com.miniproject.attendx
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.miniproject.attendx.Dashboard.Dashboard_activity
+import com.miniproject.attendx.databinding.ActivityLoginBinding
 
 
 class login : AppCompatActivity() {
-    override fun <T : View?> findViewById(id: Int): T {
-        return super.findViewById(id)
-    }
-
+    lateinit var bindingLoginPage:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
-        val buttonView = findViewById<Button>(R.id.button_login)
-        buttonView.setOnClickListener {
+        bindingLoginPage=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bindingLoginPage.root)
+        bindingLoginPage.buttonLogin.setOnClickListener {
+            val intent=Intent(this, Dashboard_activity::class.java)
+            startActivity(intent)
 
         }
+
 
     }
 
