da434d020675b7d24178e7945dbd42e4bd9468f4
Pradnyanand Jade <pradnyanand.jade@walchandsangli.ac.in>
2024-03-13 15:48:27 +0530

    Initial commit

--- app/src/main/java/com/miniproject/attendx/Dashboard_activity.kt
+++ app/src/main/java/com/miniproject/attendx/Dashboard_activity.kt
@@ -14,11 +14,13 @@ class Dashboard_activity : AppCompatActivity() {
         enableEdgeToEdge()
         ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
         }
+
     }
 }
