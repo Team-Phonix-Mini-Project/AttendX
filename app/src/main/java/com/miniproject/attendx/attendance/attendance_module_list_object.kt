eac7de0da012643cf3774f8ba33728c8ac125598
Pradnyanand jade <pradnyanand.jade@walchandsangli.ac.in>
2024-04-21 01:12:17 +0530

    final merge update  16 march 2024 AttendanceTakingActivity

--- app/src/main/java/com/miniproject/attendx/attendance/attendance_module_list_object.kt
+++
@@ -1,3 +0,0 @@
-package com.miniproject.attendx.attendance
-
-data class attendance_module_list_object(var attName:String,var attId:String)
--- app/src/main/java/com/miniproject/attendx/course_details/Activity_course_details.kt
+++ app/src/main/java/com/miniproject/attendx/course_details/Activity_course_details.kt
@@ -1,24 +1,19 @@
 package com.miniproject.attendx.course_details
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.miniproject.attendx.R
-import com.miniproject.attendx.attendance.AttendanceTakingActivity
-import com.miniproject.attendx.attendance.attendance_module_list_object
+import com.miniproject.attendx.attendance.MarkingAttDataObj
+import com.miniproject.attendx.attendance.RecordingAttendance
 import com.miniproject.attendx.databinding.ActivityCourseDetailsBinding
-import com.miniproject.attendx.databinding.AttReportModuleNameListBinding
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.FormBody
@@ -26,12 +21,20 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class activity_course_details : AppCompatActivity() {
     lateinit var binding: ActivityCourseDetailsBinding
-    var dataModuleName = arrayListOf<attendance_module_list_object>()
     lateinit var AttModID:String
+    lateinit var courseName:String
+    lateinit var sessionID:String
+    lateinit var courseID:String
+    var dataArray = arrayListOf<MarkingAttDataObj>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,13 +47,55 @@ class activity_course_details : AppCompatActivity() {
             insets
         }
 
+        courseID=intent.getStringExtra("courseid").toString()
         var courseID = intent.getStringExtra("courseid")
         var name = intent.getStringExtra("Name")
+        courseName= name.toString()
         var appl = intent.getStringExtra("User")
         appl = "Total applicants : " + appl
         binding.courseDetailsCourseName.text = name
         binding.courseDetailsApplicants.text = appl
         binding.courseDetailsTakeAttendance.setOnClickListener {
+            Toast.makeText(this@activity_course_details,"Creating session..wait", Toast.LENGTH_SHORT).show()
+            courseName= intent.getStringExtra("Name")?:""
+            courseID=intent.getStringExtra("courseid")?:""
+            fetchAttendanceModuleID(courseID.toString()){AttModID->
+                createSessionForAttendance(AttModID,courseID.toString()){sessionIDForMod->
+                    sessionID=sessionIDForMod
+                    makeUpdationsInAttendance(AttModID,courseID.toString(),sessionIDForMod){studentID,studentName,noOfUsers->
+                        Log.d("makeUpdationsInAttendance",studentName+" "+studentID+" "+noOfUsers)
+                        getStatusID(studentID,sessionIDForMod,courseID.toString(),studentName){statusIdPresent, statusset, takenByid->
+                            Log.d("getStatusID","statusid:$statusIdPresent studID:$studentID studName:$studentName")
+                            val obj = MarkingAttDataObj(
+                                courseID.toString(),
+                                AttModID,
+                                sessionID,
+                                studentName,
+                                studentID,
+                                statusIdPresent
+                            )
+                            dataArray.add(obj)
+                            Log.d("MarkingAttDataObj",obj.toString())
+                            Log.d("noOfUsers",noOfUsers+"="+dataArray.size)
+                            if(noOfUsers.toString()==dataArray.size.toString()){
+                                Log.d("noOfUsers","If condition satisfied")
+                                runOnUiThread{
+                                    binding.courseDetailsTakeAttendance.setOnClickListener {
+                                        var intent=Intent(this,
+                                            RecordingAttendance::class.java)
+                                        intent.putExtra("data",dataArray)
+                                        intent.putExtra("coursename",courseName)
+                                        startActivity(intent)
+                                    }
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+
+            }
             onTakeAttendanceButtonClicked(courseID, name)
         }
         binding.courseDetailsGetAttendanceReport.setOnClickListener {
@@ -59,10 +104,7 @@ class activity_course_details : AppCompatActivity() {
     }
 
     private fun onTakeAttendanceButtonClicked(courseID: String?, name: String?) {
-        var intent = Intent(this, AttendanceTakingActivity::class.java)
-        intent.putExtra("Name", name)
-        intent.putExtra("courseid", courseID)
-        startActivity(intent)
+
     }
 
     private fun onGetAttendanceReportClicked(courseID: String?, courseName: String?) {
@@ -76,6 +118,7 @@ class activity_course_details : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 
     private fun fetchAttendanceModuleID(courseID: String,callback: (String) -> Unit) {
         val url = "https://attendancex.moodlecloud.com/webservice/rest/server.php"
@@ -118,4 +161,144 @@ class activity_course_details : AppCompatActivity() {
         })
     }
 
+
+
+
+
+    private fun createSessionForAttendance(
+        attendanceID: String, courseID: String, callback: (String) -> Unit
+    ) {
+        val url = "https://attendancex.moodlecloud.com/webservice/rest/server.php"
+        val params = mapOf(
+            "wstoken" to "cd8c3e7ed7bf515ad9a3fec7f7f8e8ef",
+            "wsfunction" to "mod_attendance_add_session",
+            "moodlewsrestformat" to "json",
+            "attendanceid" to attendanceID,
+            "sessiontime" to getCurrentUnixTimestamp().toString()
+        )
+        val formBody = FormBody.Builder()
+        for ((key, value) in params) {
+            formBody.add(key, value)
+        }
+        val request = Request.Builder().url(url).post(formBody.build()).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string()?.let { responseBody ->
+                    val data = JSONObject(responseBody)
+                    var sessionIDForMod = data.getString("sessionid")
+                    callback(sessionIDForMod)
+                }
+            }
+
+        })
+    }
+
+    fun getCurrentUnixTimestamp(): Long {
+        return System.currentTimeMillis() / 1000
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun convertUnixTime(unixTime: Long): String {
+        val instant = Instant.ofEpochSecond(unixTime)
+        val dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return dateTime.format(formatter)
+    }
+
+    private fun makeUpdationsInAttendance(
+        attendanceID: String,
+        courseID: String,
+        sessionID: String,
+        callback: (String, String, String) -> Unit
+    ) {
+        val url = "https://attendancex.moodlecloud.com/webservice/rest/server.php"
+
+        val params = mapOf(
+            "wstoken" to "cd8c3e7ed7bf515ad9a3fec7f7f8e8ef",
+            "wsfunction" to "core_enrol_get_enrolled_users",
+            "moodlewsrestformat" to "json",
+            "courseid" to courseID
+        )
+
+        val formBody = FormBody.Builder()
+        for ((key, value) in params) {
+            formBody.add(key, value)
+        }
+
+        val request = Request.Builder().url(url).post(formBody.build()).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string()?.let { responseBody ->
+                    val users = JSONArray(responseBody)
+                    val noOfUsers = users.length().toString()
+                    var i = 0
+                    while (i < users.length()) {
+                        val user = users.getJSONObject(i)
+                        val studentID = user.getString("id")
+                        val studentName = user.getString("fullname")
+                        callback(studentID, studentName, noOfUsers.toString())
+                        i++
+                    }
+                }
+
+            }
+        })
+    }
+
+    private fun getStatusID(
+        studentID: String,
+        sessionID: String,
+        courseID: String,
+        studentName: String,
+        callback: (String, String, String) -> Unit
+    ) {
+        val url = "https://attendancex.moodlecloud.com/webservice/rest/server.php"
+
+        val params = mapOf(
+            "wstoken" to "cd8c3e7ed7bf515ad9a3fec7f7f8e8ef",
+            "wsfunction" to "mod_attendance_get_session",
+            "moodlewsrestformat" to "json",
+            "sessionid" to sessionID
+        )
+
+        val formBody = FormBody.Builder()
+        for ((key, value) in params) {
+            formBody.add(key, value)
+        }
+
+        val request = Request.Builder().url(url).post(formBody.build()).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string()?.let { responseBody ->
+                    var obj = JSONObject(responseBody)
+                    var objs = obj.getJSONArray("statuses")
+                    var x = objs.getJSONObject(0)
+                    var statusIdPresent = x.getString("id")
+                    var statusset = "1";
+                    var takenByid = studentID;
+                    callback(statusIdPresent, statusset, takenByid)
+                }
+            }
+        })
+    }
 }
